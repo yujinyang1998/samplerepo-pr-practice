c4a574a7eb03d72560bdf9befafbbddae1b2dfca
yujinyang1998 <yujinyang1998@users.noreply.github.com>
2020-08-20 18:38:35 +0800

    Added open and close messages (week 2)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,9 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!\n");
     }
 }
