540a17ad550e6b4dcd8735c9690962c45dd7e683
yujinyang1998 <yujinyang1998@users.noreply.github.com>
2020-08-27 19:22:10 +0800

    Week 3: echos commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,17 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        SystemMessages.welcomeMessage();
 
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!\n");
+        String line;
+        Scanner in = new Scanner(System.in);
+        do{
+            line = in.nextLine().trim();
+            System.out.println(line);
+
+        }while(!line.toLowerCase().equals("bye"));
+
+        SystemMessages.goodbyeMessage();
     }
 }
---
+++ src/main/java/SystemMessages.java
@@ -0,0 +1,17 @@
+public class SystemMessages {
+    public static void welcomeMessage(){
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?\n");
+    }
+
+    public static void goodbyeMessage() {
+        System.out.println("Bye. Hope to see you again soon!\n");
+    }
+}
