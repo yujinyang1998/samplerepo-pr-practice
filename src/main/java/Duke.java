36f24e0b1ea0fe8d14327a508bd54dd55ef9f383
yujinyang1998 <yujinyang1998@users.noreply.github.com>
2020-08-27 21:24:50 +0800

    Week3: Add the ability to mark tasks as done. Added a new class Task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,26 +6,62 @@ public class Duke {
     public static void main(String[] args) {
         SystemMessages.welcomeMessage();
 
-        String line;
-        Vector<String> dataStorage = new Vector<String>();
+        boolean breakCheck = false;
+        String input,firstWord;
+        Vector<Task> dataStorage = new Vector<Task>();
+        int counter = 1;
         Scanner in = new Scanner(System.in);
-        do{
-            line = in.nextLine().trim();
 
-            if(line.toLowerCase().equals("list")){
-                Iterator<String> i = dataStorage.iterator();
-                int counter = 1;
-                while(i.hasNext()){
-                    System.out.println(counter + ": " + i.next());
+        while(breakCheck == false) {
+            input = in.nextLine().trim();
+            firstWord = input.split(" ",2)[0];
+
+            switch (firstWord.toLowerCase()) {
+                case "bye":
+                    breakCheck = true;
+                    break;
+
+                case "":
+                    System.out.println("Please enter something.");
+                    break;
+
+                case "list":
+                    Iterator<Task> i = dataStorage.iterator();
+                    int listCounter = 1;
+                    while(i.hasNext()){
+                        Task printTask = i.next();
+                        System.out.println(listCounter + ".[" + printTask.getStatusIcon() + "] " + printTask.description);
+                        listCounter++;
+                    }
+                    break;
+
+                case "done":
+                    if(input.split(" ").length>2 || !input.split(" ")[1].matches("^-?\\d+$")){
+                        System.out.println("Invalid input.");
+                        break;
+                    }
+                    int completedIndex = Integer.parseInt(input.split(" ")[1].trim());
+
+                    if(completedIndex <= counter && !dataStorage.get(completedIndex - 1).isDone){
+                        Task replacement = new Task(dataStorage.get(completedIndex-1).description);
+                        replacement.isDone = true;
+                        dataStorage.set(completedIndex-1,replacement);
+                        System.out.println("Nice! I've marked this task as done: ");
+                        System.out.println("[" + dataStorage.get(completedIndex-1).getStatusIcon() + "]" + dataStorage.get(completedIndex-1).description);
+                    }else if(completedIndex <= counter && dataStorage.get(completedIndex - 1).isDone){
+                        System.out.println("The task has already been marked as completed.");
+                    }else{
+                        System.out.println("This task does not exist.");
+                    }
+                    break;
+
+                default:
+                    Task newTask = new Task(input);
+                    dataStorage.add(newTask);
+                    System.out.println("added: " + input);
                     counter++;
-                }
-            }else if(line.trim().equals("")){
-                System.out.println("Please enter something.");
-            }else if(!line.toLowerCase().equals("bye")){
-                dataStorage.add(line);
-                System.out.println("added: " + line);
             }
-        }while(!line.toLowerCase().equals("bye"));
+        }
 
         SystemMessages.goodbyeMessage();
     }
