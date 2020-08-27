c896fded013294299ef16352be6bc2c6f8bdf6bf
yujinyang1998 <yujinyang1998@users.noreply.github.com>
2020-08-27 19:43:13 +0800

    Week3 Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,30 @@
+import java.util.Iterator;
 import java.util.Scanner;
+import java.util.Vector;
 
 public class Duke {
     public static void main(String[] args) {
         SystemMessages.welcomeMessage();
 
         String line;
+        Vector<String> dataStorage = new Vector<String>();
         Scanner in = new Scanner(System.in);
         do{
             line = in.nextLine().trim();
-            System.out.println(line);
 
+            if(line.toLowerCase().equals("list")){
+                Iterator<String> i = dataStorage.iterator();
+                int counter = 1;
+                while(i.hasNext()){
+                    System.out.println(counter + ": " + i.next());
+                    counter++;
+                }
+            }else if(line.trim().equals("")){
+                System.out.println("Please enter something.");
+            }else if(!line.toLowerCase().equals("bye")){
+                dataStorage.add(line);
+                System.out.println("added: " + line);
+            }
         }while(!line.toLowerCase().equals("bye"));
 
         SystemMessages.goodbyeMessage();
